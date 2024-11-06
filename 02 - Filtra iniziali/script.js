c3dd72178ade47896a60224825720b267f9f52da
vincenzobozzanca <vinci.boz@gmail.com>
2024-11-06 16:24:59 +0100

    2 COMPLETP

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -6,8 +6,27 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 // Dichiara la funzione qui.
 
 
-// Invoca la funzione qui e stampa il risultato in console
+let charSelector = (array, character) => {
+    let new_array = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
 
+        // creo una variabile per gli elementi che iniziano con il carattere A
+        let specificChar = element.startsWith(character);
+
+        // se iniziano con il carattere specifico
+        if (specificChar) {   
+            // pusha l'elemento dentro al new_array
+            new_array.push(element);
+        }
+    }
+
+    return new_array;
+}
+
+console.log(charSelector(names, "A"));
+// Invoca la funzione qui e stampa il risultato in console
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
