2ba78a1eb4d57d57d8920799faadc1a4ac0dcb34
vincenzobozzanca <vinci.boz@gmail.com>
2024-11-06 16:35:35 +0100

    0.4

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -5,9 +5,9 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+let saluti = (word) => `Ciao ${word}`;
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
-
+console.log(saluti(name));
 //Risultato atteso se si passa 'Mario': // ciao Mario
