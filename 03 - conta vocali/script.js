3f743325c27496da9710e2aa6f68c4177ab30d7d
vincenzobozzanca <vinci.boz@gmail.com>
2024-11-06 16:33:00 +0100

    03 completo

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,28 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function numeroVocali(string) {
 
+    
+    let vocali = ["a", "e", "i", "o", "u",];
+
+    let contaVocali = 0;
+    
+    for (let i = 0; i < string.length; i++) {
+        const element = string[i];
+
+        if (vocali.includes(element)) {
+            // incremeto il contatore di 1
+            contaVocali++;
+            console.log(element);
+        } 
+    }
+    
+    console.log(contaVocali);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+numeroVocali(word);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
